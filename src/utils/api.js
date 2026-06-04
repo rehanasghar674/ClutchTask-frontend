14e893dda6ff0450ce6a2fa1f2a2ff57b3a74f58
Rehan Asghar <rehan79664703@gmail.com>
2026-06-05 00:16:43 +0500

    update api js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:5000/api'
+const baseURL = 'http://clutch-task-backend.vercel.app/api'
 
 export const api = axios.create({
   baseURL: baseURL,
