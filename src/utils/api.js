e1c88e1252b5ecc0ec9f1fdeeec6d69438832b61
Rehan Asghar <rehan79664703@gmail.com>
2026-06-05 20:18:11 +0500

    new update data

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,5 +4,6 @@ const baseURL = 'https://clutch-task-backend.vercel.app/api'
 
 export const api = axios.create({
   baseURL: baseURL,
+  credentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
